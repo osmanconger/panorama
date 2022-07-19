cf08e5a8751f794412a5b44b700115614f76b93c
Osman Conger <osmansconger@gmail.com>
2022-07-18 21:16:44 -0400

    chore: add host env var to cors

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://" + process.env.HOST + ":3000",
     methods: "GET,POST,PUT,DELETE",
     credentials: true
   })
